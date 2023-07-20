134e1135c944fe59df25c3cc8f5a52fd12e2bf43
Tomás Rueda <tomasdrz@gmail.com>
2023-07-20 22:32:02 +0200

    otra importación de react

--- src/app/listuser/page.tsx
+++ src/app/listuser/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import {
   containsFilterState,
